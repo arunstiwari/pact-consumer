1501dec543f75b08ee0ee8d023556fbf16dbe983
arun.tiwari123 <arunstiwari@gmail.com>
2025-03-05 21:13:52 +0530

    Added the consumer side pact test

--- src/main/java/com/example/consumer/service/ProductServiceClient.java
+++ src/main/java/com/example/consumer/service/ProductServiceClient.java
@@ -1,7 +1,12 @@
 package com.example.consumer.service;
 
+import com.example.consumer.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +20,18 @@ public class ProductServiceClient {
 
     public ProductResponse fetchProducts(){
         return restTemplate.getForObject(baseUrl+"/products", ProductResponse.class);
+    }
+
+    public Product fetchProductById(Long id){
+        return restTemplate.getForObject(baseUrl+"/products/"+id, Product.class);
+    }
+
+    public Product createProduct(Product product){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Product> request = new HttpEntity<>(product, headers);
+        ResponseEntity<Product> response = restTemplate.postForEntity(baseUrl + "/product", request, Product.class);
+        return response.getBody();
     }
 
     public void setBaseUrl(String url) {
